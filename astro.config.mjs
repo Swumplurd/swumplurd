d78718324e10aefc77c4484d2f26123278b7c86f
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2024-10-13 18:13:52 -0600

    config: gh pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'https://swumplurd.github.io/swumplurd/',
+  base: '/swumplurd/',
   output: 'static',
   integrations: [tailwind(), react(), icon(), mdx()]
 });
