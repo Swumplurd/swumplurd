74f02fef315fad3027755c0104dd6f46853c20a0
Fernando Vasquez <swumplurd@outlook.com>
2024-10-14 11:05:36 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,6 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: "https://swumplurd.github.io",
-  base: '/swumplurd',
   output: 'static',
   integrations: [tailwind(), react(), icon(), mdx()]
 });
